8ed38006fbc4db974e5a8b7434db8e36015d5c51
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-04-11 14:24:31 +0700

    Update run.bat

--- Seismic/run.bat
+++ Seismic/run.bat
@@ -6,21 +6,6 @@ cd %~dp0
 
 echo Checking configuration files...
 
-if not exist configs.json (
-    echo {> configs.json
-    echo   "timeZone": "en-US",>> configs.json
-    echo   "rotateProxy": false,>> configs.json
-    echo   "skipInvalidProxy": false,>> configs.json
-    echo   "proxyRotationInterval": 2,>> configs.json
-    echo   "delayEachAccount": [5, 8],>> configs.json
-    echo   "timeToRestartAllAccounts": 300,>> configs.json
-    echo   "howManyAccountsRunInOneTime": 100,>> configs.json
-    echo   "doTasks": true,>> configs.json
-    echo   "playGames": true,>> configs.json
-    echo   "referralCode": "">> configs.json
-    echo }>> configs.json
-    echo Created configs.json
-)
 
 (for %%F in (privateKeys.txt proxies.txt) do (
     if not exist %%F (
@@ -35,11 +20,11 @@ echo Checking dependencies...
 if exist "..\node_modules" (
     echo Using node_modules from parent directory...
     cd ..
-    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent ethers web3 ws uuid xlsx readline-sync moment lodash
+    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent ethers web3 solc
     cd %~dp0
 ) else (
     echo Installing dependencies in current directory...
-    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent ethers web3 ws uuid xlsx readline-sync moment lodash
+    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent ethers web3 solc
 )
 echo Dependencies installation completed!
 
